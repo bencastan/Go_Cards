556bc847ecc8cff686dbe497b5d50795d5584d1d
Ben Castan <benc@rMBP-16.local>
2022-03-06 14:53:42 +1100

    Section 13

--- main.go
+++ main.go
@@ -1,14 +1,6 @@
 package main
 
-//import "fmt"
-
 func main() {
 	cards := newDeck()
-
-	hand, remainingDeck := deal(cards, 5)
-
-	hand.print()
-	remainingDeck.print()
+	cards.saveToFile("my_cards")
 }
-
-
